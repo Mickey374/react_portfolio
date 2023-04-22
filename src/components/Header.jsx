16d9ef6dc1177a57aa5b8ee5e157f1238b282670
Mickey374 <asantemichael37483@gmail.com>
2023-04-23 01:47:27 +0200

    Integrated the Github API to get data based on username

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
 import "./Header.css";
-import Link from "./Link";
+// import Link from "./Link";
 
 const Header = ({ logo }) => {
     return (
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload
-            </p>
-            {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a> */}
-            <Link title="Learn React" url="https://reactjs.org" />
+            <h1>Mike's Portfolio</h1>
         </header>
     );
 };
