0c6ea7faf0450d4afd2eba5b2ec6abaab0b49886
Mickey374 <asantemichael37483@gmail.com>
2023-04-23 15:00:01 +0200

    Added a Page and route for path not exists.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Projects from "./pages/Projects";
+import NotFound from "./pages/NotFound";
 import logo from "./assets/logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Profile userName="Mickey374" />} />
           <Route path="/projects" element={<Projects userName="Mickey374" />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import "./NotFound.css";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <img src="https://i.imgur.com/qIufhof.png" alt="404 - Page Not Found" />
+            <p>The page you're looking for does not exist.</p>
+        </div>
+    )
+}
+
+export default NotFound
