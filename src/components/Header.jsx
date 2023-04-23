05bb8cb53207ead54ee08e7bb6f07a919d0139f3
Mickey374 <asantemichael37483@gmail.com>
2023-04-23 15:25:02 +0200

    Added link in header for routing.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import "./Header.css";
-// import Link from "./Link";
+import { Link as RouterLink } from "react-router-dom";
 
 const Header = ({ logo }) => {
     return (
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Mike's Portfolio</h1>
+            <nav>
+                <RouterLink to="/" className="App-link">About Me</RouterLink>
+                <RouterLink to="/projects" className="App-link">Projects</RouterLink>
+            </nav>
         </header>
     );
 };
