2a9f910355277ef70e9fd7740eb7981f4f79d4b1
Mickey374 <asantemichael37483@gmail.com>
2023-04-23 14:22:40 +0200

    Added routing with React router in App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import logo from "./assets/logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header logo={logo} />
-      <Profile userName="Mickey374" />
+      <BrowserRouter>
+        <Header logo={logo} />
+        {/* <Profile userName="Mickey374" /> */}
+        <Routes>
+          <Route path="/" element={<Profile userName="Mickey374" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
